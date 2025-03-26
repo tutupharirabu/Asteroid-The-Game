13c332bb88e530174d445b2fa5f40e66b07ef3b2
Irfan Zharauri Nanda Sudiyanto <code.zharaurien@gmail.com>
2025-03-26 07:31:38 +0700

    first step to produce my first game on python

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+def main():
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  while True:
+    pygame.Surface.fill(screen, (0, 0, 0))
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
+
+if __name__ == "__main__":
+  main()
